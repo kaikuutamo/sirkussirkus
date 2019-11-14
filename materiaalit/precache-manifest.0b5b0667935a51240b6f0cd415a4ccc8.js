5627ea520d9330c0246a4654399c02c83fb89bf9
kaikuutamo <kaitsu@sirkussirkus.com>
2019-11-14 11:40:43 +0200

    kaikuutamo14112019

---
+++ materiaalit/precache-manifest.0b5b0667935a51240b6f0cd415a4ccc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "642ac7e151f4b21dfce6c504dea83756",
+    "url": "/static/media/shopping-cart.642ac7e1.png"
+  },
+  {
+    "revision": "3d44414cfbc03cfc3e39a21751a05db5",
+    "url": "/static/media/logo3.3d44414c.png"
+  },
+  {
+    "revision": "fa267c65b95a134af92bb5e3dfef5451",
+    "url": "/static/media/banner4.fa267c65.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4b069a671c49292bcb57",
+    "url": "/static/js/main.4b069a67.chunk.js"
+  },
+  {
+    "revision": "5f3ffab4879dc0a66535",
+    "url": "/static/js/2.5f3ffab4.chunk.js"
+  },
+  {
+    "revision": "4b069a671c49292bcb57",
+    "url": "/static/css/main.7cd6fdc3.chunk.css"
+  },
+  {
+    "revision": "348de6347246621bfa47f5dda5b0cf82",
+    "url": "/index.html"
+  }
+];
---
+++ materiaalit/precache-manifest.8d18ed8c10b20befaf505158bea6e02b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "642ac7e151f4b21dfce6c504dea83756",
+    "url": "/static/media/shopping-cart.642ac7e1.png"
+  },
+  {
+    "revision": "3d44414cfbc03cfc3e39a21751a05db5",
+    "url": "/static/media/logo3.3d44414c.png"
+  },
+  {
+    "revision": "fa267c65b95a134af92bb5e3dfef5451",
+    "url": "/static/media/banner4.fa267c65.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "aef329e299b5aa28a68d",
+    "url": "/static/js/main.aef329e2.chunk.js"
+  },
+  {
+    "revision": "5f3ffab4879dc0a66535",
+    "url": "/static/js/2.5f3ffab4.chunk.js"
+  },
+  {
+    "revision": "aef329e299b5aa28a68d",
+    "url": "/static/css/main.53923b6e.chunk.css"
+  },
+  {
+    "revision": "d9150c63633ab03c1c02f6c102fc1533",
+    "url": "/index.html"
+  }
+];
